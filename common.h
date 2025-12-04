c935dd335febef8f8df4cc8af8f7e4c9764c8525
Denny Page <dennypage@me.com>
2025-12-04 08:52:47 -0800

    Update version to 1.2.0.

--- common.h
+++ common.h
@@ -40,7 +40,7 @@
 
 
 // Version number of mcast-bridge
-#define VERSION                 "1.0.0"
+#define VERSION                 "1.2.0"
 
 
 // Maximum packet size for UDP packet we support. Note that this limit
